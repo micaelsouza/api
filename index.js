412d8a90c2dc50a717464416ceecc94895dd21c0
micaelsouza <micael.souza@outlook.com>
2016-12-12 14:57:25 -0300

    CORS

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/api/saldo', function(req, res) {
   if (!req.body.card || !req.body.document) return res.sendStatus(400);
 
